84e7f7350e9ea3f1d070a4d8db7201d0024cdd1a
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-11-06 22:17:35 +0530

    First Commt

---
+++ app/controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: lalittanwar
+ * Date: 06/11/15
+ * Time: 3:52 PM
+ */
+
+namespace app\controller {
+
+    class MainController
+    {
+
+    }
+
+}
+
